3412eb84a1138a35273976d64d37265218061d02
Maksim Efremov <ma-efremoff@yandex-team.ru>
2024-07-21 16:58:21 +0300

    chore: update storybook v8

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,19 +1,26 @@
 import type {StorybookConfig} from '@storybook/react-webpack5';
 const config: StorybookConfig = {
     stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
+
     addons: [
         '@storybook/addon-links',
         {name: '@storybook/addon-essentials', options: {backgrounds: false}},
         '@storybook/addon-interactions',
         '@storybook/preset-scss',
+        '@storybook/addon-webpack5-compiler-babel'
     ],
+
     framework: {
         name: '@storybook/react-webpack5',
         options: {fastRefresh: true},
     },
+
     docs: {
-        autodocs: 'tag',
-        defaultName: 'Docs',
+        defaultName: 'Docs'
     },
+
+    typescript: {
+        reactDocgen: 'react-docgen-typescript'
+    }
 };
 export default config;
